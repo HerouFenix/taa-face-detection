cb5276a42e0e09d721d2f8f7c288480526d4b672
DrPunpun <pedrooliveira99@ua.pt>
2020-06-21 11:12:14 +0100

    models trained

--- ImageResources/Dataset/WIDER_train/cropped_images/cascadetrainer.sh
+++ ImageResources/Dataset/WIDER_train/cropped_images/cascadetrainer.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-for no_stages in 1 10 20 30 40 50
-do
-    mkdir "model${no_stages}stages"
-
-    start=$SECONDS
-    opencv_traincascade -data "model${no_stages}stages" -vec faces.vec -bg negative_info.txt -numPos 1500 -numNeg 1500 -numStages $no_stages -w 24 -h 24 
-    duration=$(( SECONDS - start ))
-
-    echo $no_stages : $duration > "${no_stages}_Time"
-done
-
-
-#for no_stages in 5 15 25 35 45
+#for no_stages in 1 10 20 30 40 50
 #do
 #    mkdir "model${no_stages}stages"
 #
@@ -22,3 +10,15 @@ done
 #
 #    echo $no_stages : $duration > "${no_stages}_Time"
 #done
+
+
+for no_stages in 5 15 25 35 45
+do
+    mkdir "model${no_stages}stages"
+
+    start=$SECONDS
+    opencv_traincascade -data "model${no_stages}stages" -vec faces.vec -bg negative_info.txt -numPos 1500 -numNeg 1500 -numStages $no_stages -numThreads 10 -w 24 -h 24 
+    duration=$(( SECONDS - start ))
+
+    echo $no_stages : $duration > "${no_stages}_Time.txt"
+done
